50cfaa7a431874a2fabf546a1b0e2ce1bce0f132
Alysson Batista <alyssonbatista241@gmail.com>
2021-04-20 23:53:31 -0300

    Update resp2_a.js

--- cap2/exercícios/js/resp2_a.js
+++ cap2/exercícios/js/resp2_a.js
@@ -1,16 +1,23 @@
 function desconto(){
+   
+    //Cria referência ao elemento da página
     var inMedicamento = document.getElementById("inNomeMedicamento");
     var inPreco = document.getElementById("inPreco");
     var outDesconto = document.getElementById("outDesconto");
     var outNome = document.getElementById("outNome");
 
+    //obtém conteúdos dos campos de entrada
     var nome = inMedicamento.value;
     var preco = Number(inPreco.value);
-    var desconto = parseInt(preco) * 2;
 
+    //calcula o desconto do cliente
+    var desconto = parseInt(preco) * 2;
+    
+    //altera o conteúdo da linha de resposta
     outNome.textContent = "Promoção de " + nome; 
     outDesconto.textContent = "Leve 2 por apenas R$: " + desconto.toFixed(2)
 }
-
+//cria referência ao botão clicar
 var promocao = document.getElementById("processamentoPreco");
+//registra um evento associado ao botão pra carregar uma função
 promocao.addEventListener("click",desconto);
