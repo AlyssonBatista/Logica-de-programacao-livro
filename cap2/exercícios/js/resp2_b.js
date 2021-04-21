574a1970dd43a82921a7bf3fc4f211daa67aae64
Alysson Batista <alyssonbatista241@gmail.com>
2021-04-20 23:54:20 -0300

    Update resp2_b.js

--- cap2/exercícios/js/resp2_b.js
+++ cap2/exercícios/js/resp2_b.js
@@ -7,6 +7,7 @@ function preco(){
     //obtem o conteudo do campo de entrada
     var valor = Number(inValor.value);// valor por 15 minutos
     var tempo = Number(inTempo.value);// tempo de uso
+
     //calcula o valor a ser pago pelo cliente
     var precoCliente = Math.ceil(tempo / 15) * valor;// math.ceil arredonda pra cima
 
@@ -15,5 +16,6 @@ function preco(){
 }
 //Cria referência ao botão clicar
 var outPreco = document.getElementById("calcula");
+
 //registra um evento associado ao botão pra carregar uma função
 outPreco.addEventListener("click",preco);
